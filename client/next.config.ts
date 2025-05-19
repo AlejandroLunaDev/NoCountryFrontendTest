41cfe9bbc91377e216cb2292df5d95285a226374
AlejandroLunaDev <alejandrolunadev@gmail.com>
2025-05-19 14:24:56 -0300

    Update Next.js configuration to enhance API route handling
    - Modified the serverRuntimeConfig to be empty and added output as 'standalone'.
    - Updated the rewrites function to conditionally return API routes only in non-production environments, improving flexibility and maintainability of API routing.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverRuntimeConfig: {
-    PORT: 3001
-  },
+  output: 'standalone',
+  serverRuntimeConfig: {},
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://nocountrytest.onrender.com/api/:path*'
-      }
-    ];
+    if (process.env.NODE_ENV !== 'production') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://nocountrytest.onrender.com/api/:path*'
+        }
+      ];
+    }
+    return [];
   }
   /* config options here */
 };
